8bf98bbb34bd04778a610d26e9b9d26435a9c652
Vitor Bastos <vitorbastosbn@gmail.com>
2023-06-05 17:34:00 -0300

    #1 - Projeto Base e dependências

---
+++ src/main/java/com/desafio/pauta/DesafioDBCSicrediApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.pauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDBCSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDBCSicrediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/pauta/DesafioDBCSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pauta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDBCSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
